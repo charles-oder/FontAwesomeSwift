5c2c3737c3a77643dce9543d2a0dccb6ed963891
Charles Oder <charles.oder@dtn.com>
2016-12-26 19:13:50 -0600

    Initial Commit

---
+++ FontAwesomeSwift/FontAwesomeSwift.h
@@ -0,0 +1,19 @@
+//
+//  FontAwesomeSwift.h
+//  FontAwesomeSwift
+//
+//  Created by Charles Oder Dev on 12/26/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FontAwesomeSwift.
+FOUNDATION_EXPORT double FontAwesomeSwiftVersionNumber;
+
+//! Project version string for FontAwesomeSwift.
+FOUNDATION_EXPORT const unsigned char FontAwesomeSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FontAwesomeSwift/PublicHeader.h>
+
+
