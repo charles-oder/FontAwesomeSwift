5305ad678fbdf8b615d181b7d800e3ddccf0c8a7
Charles Oder <charles.oder@dtn.com>
2016-12-28 11:52:32 -0600

    Added Zocial icons

---
+++ FontAwesomeSwift/FASZocial.swift
@@ -0,0 +1,221 @@
+//
+//  FASZocial.swift
+//  FontAwesomeSwift
+//
+//  Created by Charles Oder Dev on 12/28/16.
+//
+//
+
+import Foundation
+
+public class FASZocial: FASFont {
+    
+    public override func font(size: CGFloat) -> UIFont {
+        return loadFont(name: "Zocial", type: "ttf", size: size)
+    }
+    
+    public func acrobatIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "\u{00E3}") }
+    public func amazonIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "a") }
+    public func androidIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "&") }
+    public func angellistIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "\u{00D6}") }
+    public func aolIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "\"") }
+    public func appnetIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "\u{00E1}") }
+    public func appstoreIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "A") }
+    public func bitbucketIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "\u{00E9}") }
+    public func bitcoinIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "2") }
+    public func bloggerIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "B") }
+    public func bufferIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "\u{00E5}") }
+    public func callIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "7") }
+    public func calIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: ".") }
+    public func cartIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "\u{00C9}") }
+    public func chromeIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "[") }
+    public func cloudappIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "c") }
+    public func creativecommonsIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "C") }
+    public func deliciousIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "#") }
+    public func diggIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: ";") }
+    public func disqusIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "Q") }
+    public func dribbbleIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "D") }
+    public func dropboxIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "d") }
+    public func drupalIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "\u{00E4}") }
+    public func dwollaIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "\u{00E0}") }
+    public func emailIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "]") }
+    public func eventasaurusIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "v") }
+    public func eventbriteIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "|") }
+    public func eventfulIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "'") }
+    public func evernoteIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "E") }
+    public func facebookIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "f") }
+    public func fivehundredpxIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "0") }
+    public func flattrIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "%") }
+    public func flickrIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "F") }
+    public func forrstIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: ":") }
+    public func foursquareIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "4") }
+    public func githubIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "\u{00E8}") }
+    public func gmailIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "m") }
+    public func googleIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "G") }
+    public func googleplayIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "h") }
+    public func googleplusIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "+") }
+    public func gowallaIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "@") }
+    public func groovesharkIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "8") }
+    public func guestIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "?") }
+    public func html5Icon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "5") }
+    public func ieIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "6") }
+    public func instagramIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "\u{00DC}") }
+    public func instapaperIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "I") }
+    public func intensedebateIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "{") }
+    public func itunesIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "i") }
+    public func kloutIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "K") }
+    public func lanyrdIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "-") }
+    public func lastfmIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "l") }
+    public func legoIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "\u{00EA}") }
+    public func linkedinIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "L") }
+    public func lkdtoIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "\u{00EE}") }
+    public func logmeinIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "\u{00EB}") }
+    public func macstoreIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "^") }
+    public func meetupIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "M") }
+    public func myspaceIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "_") }
+    public func ninetyninedesignsIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "9") }
+    public func openidIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "o") }
+    public func opentableIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "\u{00C7}") }
+    public func paypalIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "$") }
+    public func pinboardIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "n") }
+    public func pinterestIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "1") }
+    public func plancastIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "P") }
+    public func plurkIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "j") }
+    public func pocketIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "\u{00E7}") }
+    public func podcastIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "`") }
+    public func posterousIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "~") }
+    public func printIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "\u{00D1}") }
+    public func quoraIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "q") }
+    public func redditIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: ">") }
+    public func rssIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "R") }
+    public func scribdIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "}") }
+    public func skypeIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "S") }
+    public func smashingIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "*") }
+    public func songkickIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "k") }
+    public func soundcloudIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "s") }
+    public func spotifyIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "=") }
+    public func stackoverflowIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "\u{00EC}") }
+    public func statusnetIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "\u{00E2}") }
+    public func steamIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "b") }
+    public func stripeIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "\u{00A3}") }
+    public func stumbleuponIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "/") }
+    public func tumblrIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "t") }
+    public func twitterIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "T") }
+    public func viadeoIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "H") }
+    public func vimeoIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "V") }
+    public func vkIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "N") }
+    public func weiboIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "J") }
+    public func wikipediaIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: ",") }
+    public func windowsIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "W") }
+    public func wordpressIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "w") }
+    public func xingIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "X") }
+    public func yahooIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "Y") }
+    public func ycombinatorIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "\u{00ED}") }
+    public func yelpIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "y") }
+    public func youtubeIcon(size: CGFloat) -> FASIcon { return FASIcon(font: font(size: size), iconCode: "U") }
+    
+    public var allIcons: [String : String] {
+        return [
+            "zocial.acrobat" : "\u{00E3}",
+            "zocial.amazon" : "a",
+            "zocial.android" : "&",
+            "zocial.angellist" : "\u{00D6}",
+            "zocial.aol" : "\"",
+            "zocial.appnet" : "\u{00E1}",
+            "zocial.appstore" : "A",
+            "zocial.bitbucket" : "\u{00E9}",
+            "zocial.bitcoin" : "2",
+            "zocial.blogger" : "B",
+            "zocial.buffer" : "\u{00E5}",
+            "zocial.call" : "7",
+            "zocial.cal" : ".",
+            "zocial.cart" : "\u{00C9}",
+            "zocial.chrome" : "[",
+            "zocial.cloudapp" : "c",
+            "zocial.creativecommons" : "C",
+            "zocial.delicious" : "#",
+            "zocial.digg" : ";",
+            "zocial.disqus" : "Q",
+            "zocial.dribbble" : "D",
+            "zocial.dropbox" : "d",
+            "zocial.drupal" : "\u{00E4}",
+            "zocial.dwolla" : "\u{00E0}",
+            "zocial.email" : "]",
+            "zocial.eventasaurus" : "v",
+            "zocial.eventbrite" : "|",
+            "zocial.eventful" : "'",
+            "zocial.evernote" : "E",
+            "zocial.facebook" : "f",
+            "zocial.fivehundredpx" : "0",
+            "zocial.flattr" : "%",
+            "zocial.flickr" : "F",
+            "zocial.forrst" : ":",
+            "zocial.foursquare" : "4",
+            "zocial.github" : "\u{00E8}",
+            "zocial.gmail" : "m",
+            "zocial.google" : "G",
+            "zocial.googleplay" : "h",
+            "zocial.googleplus" : "+",
+            "zocial.gowalla" : "@",
+            "zocial.grooveshark" : "8",
+            "zocial.guest" : "?",
+            "zocial.html5" : "5",
+            "zocial.ie" : "6",
+            "zocial.instagram" : "\u{00DC}",
+            "zocial.instapaper" : "I",
+            "zocial.intensedebate" : "{",
+            "zocial.itunes" : "i",
+            "zocial.klout" : "K",
+            "zocial.lanyrd" : "-",
+            "zocial.lastfm" : "l",
+            "zocial.lego" : "\u{00EA}",
+            "zocial.linkedin" : "L",
+            "zocial.lkdto" : "\u{00EE}",
+            "zocial.logmein" : "\u{00EB}",
+            "zocial.macstore" : "^",
+            "zocial.meetup" : "M",
+            "zocial.myspace" : "_",
+            "zocial.ninetyninedesigns" : "9",
+            "zocial.openid" : "o",
+            "zocial.opentable" : "\u{00C7}",
+            "zocial.paypal" : "$",
+            "zocial.pinboard" : "n",
+            "zocial.pinterest" : "1",
+            "zocial.plancast" : "P",
+            "zocial.plurk" : "j",
+            "zocial.pocket" : "\u{00E7}",
+            "zocial.podcast" : "`",
+            "zocial.posterous" : "~",
+            "zocial.print" : "\u{00D1}",
+            "zocial.quora" : "q",
+            "zocial.reddit" : ">",
+            "zocial.rss" : "R",
+            "zocial.scribd" : "}",
+            "zocial.skype" : "S",
+            "zocial.smashing" : "*",
+            "zocial.songkick" : "k",
+            "zocial.soundcloud" : "s",
+            "zocial.spotify" : "=",
+            "zocial.stackoverflow" : "\u{00EC}",
+            "zocial.statusnet" : "\u{00E2}",
+            "zocial.steam" : "b",
+            "zocial.stripe" : "\u{00A3}",
+            "zocial.stumbleupon" : "/",
+            "zocial.tumblr" : "t",
+            "zocial.twitter" : "T",
+            "zocial.viadeo" : "H",
+            "zocial.vimeo" : "V",
+            "zocial.vk" : "N",
+            "zocial.weibo" : "J",
+            "zocial.wikipedia" : ",",
+            "zocial.windows" : "W",
+            "zocial.wordpress" : "w",
+            "zocial.xing" : "X",
+            "zocial.yahoo" : "Y",
+            "zocial.ycombinator" : "\u{00ED}",
+            "zocial.yelp" : "y",
+            "zocial.youtube" : "U",
+            
+        ]
+    }
+}
