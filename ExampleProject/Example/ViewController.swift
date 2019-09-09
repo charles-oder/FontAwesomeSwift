baf989a4f0800beb4891c4e7307b20b126212b28
Charles <charles.oder@ehi.com>
2019-09-09 12:17:38 -0500

    added example project

---
+++ ExampleProject/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Charles Oder on 9/9/19.
+//  Copyright © 2019 Charles Oder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ExampleProject/Pods/Target Support Files/FontAwesomeSwift/FontAwesomeSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FontAwesomeSwift : NSObject
+@end
+@implementation PodsDummy_FontAwesomeSwift
+@end
